3ffaa580fdb0369b4b0710e76920015e1130c625
Siddhartha Singh <sidsiskilled@gmail.com>
2024-02-07 23:24:14 +0530

    Have handeled input output in makefile and main

--- makefile
+++ makefile
@@ -1,13 +1,43 @@
 Compiler=g++
-Flags=-c -Wall
+Flags=-std=c++20 -c -Wall
 BinFolder=./bin/
 SrcFolder=./src/
 
 all:buildProg
-
+ifeq ($(strategy),BASIC)
+	./main $(strategy) $(symbol) $(n) $(x) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),DMA)
+	./main $(strategy) $(symbol) $(n) $(x) $(p) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),DMA++)
+	./main $(strategy) $(symbol) $(x) $(p) $(n) $(max_hold_days) $(c1) $(c2) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),MACD)
+	./main $(strategy) $(symbol) $(x) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),RSI)
+	./main $(strategy) $(symbol) $(x) $(n) $(oversold_threshold) $(overbought_threshold) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),ADX)
+	./main $(strategy) $(symbol) $(x) $(n) $(adx_threshold) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),LINEAR_REGRESSION)
+	./main $(strategy) $(symbol) $(x) $(p) $(train_start_date) $(train_end_date) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),BEST_OF_ALL)
+	./main $(strategy) $(symbol) $(start_date) $(end_date)
+endif
+ifeq ($(strategy),PAIRS)
+ifndef stop_loss_threshold
+	./main $(strategy) $(symbol1) $(symbol2) $(x) $(n) $(threshold) $(start_date) $(end_date)
+else
+	./main $(strategy) $(symbol1) $(symbol2) $(x) $(n) $(threshold) $(start_date) $(end_date) $(stop_loss_threshold)
+endif
+endif
 buildProg: main.o
-	$(Compiler) main.o -o main
-main.o: main.cpp
+	$(Compiler) $(BinFolder)main.o -o main
+main.o: $(SrcFolder)main.cpp
 	$(Compiler) $(Flags) $(SrcFolder)main.cpp -o $(BinFolder)main.o
 clearAll: clearObjs clearExe
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,222 @@
 #include <iostream>
+#include <string>
+#include <chrono>
 
+using namespace std;
 
+chrono::year_month_day str2Date(string& date){
+    int day = stoi(date.substr(0,2));
+    int month = stoi(date.substr(3,2));
+    int year = stoi(date.substr(6,4));
+    return chrono::year_month_day(chrono::year(year),chrono::month(month),chrono::day(day));
+}
+
+void handleBASIC(int argc,char* argv[]){
+    string symbol(argv[2]);
+    int n = stoi(argv[3]);
+    int x = stoi(argv[4]);
+    string startDateStr(argv[5]);
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    string endDateStr(argv[6]);
+    chrono::year_month_day endDate = str2Date(endDateStr);
+    
+    //cout << "symbol is:" << symbol << endl;
+    //cout << "n is:" << n << endl;
+    //cout << "x is:" << x << endl;
+    //cout << "startDate str is:" << startDateStr << endl;
+    //cout << "endDate str is:" << endDateStr << endl;
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;
+    
+}
+void handleDMA(int argc,char* argv[]){
+    string symbol(argv[2]);
+    int n = stoi(argv[3]);
+    int x = stoi(argv[4]);
+    int p = stoi(argv[5]);
+    string startDateStr(argv[6]);
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    string endDateStr(argv[7]);
+    chrono::year_month_day endDate = str2Date(endDateStr);
+    
+    //cout << "symbol is:" << symbol << endl;
+    //cout << "n is:" << n << endl;
+    //cout << "x is:" << x << endl;
+    //cout << "p is:" << p << endl;
+    //cout << "startDate str is:" << startDateStr << endl;
+    //cout << "endDate str is:" << endDateStr << endl;
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;
+}
+void handleDMAplus(int argc,char* argv[]){
+    //cout << "Entered DMA++" << endl;
+    string symbol(argv[2]);
+    //cout << "symbol is:" << symbol << endl;
+    int x = stoi(argv[3]);
+    //cout << "x is:" << x << endl;
+    int p = stoi(argv[4]);
+    //cout << "p is:" << p << endl;
+    int n = stoi(argv[5]);
+    //cout << "n is:" << n << endl;
+    int max_hold_days = stoi(argv[6]);
+    //cout << "max_hold_days:" << max_hold_days << endl;
+    float c1 = stof(argv[7]);
+    //cout << "c1:" << c1 << endl;
+    float c2 = stof(argv[8]);
+    //cout << "c2:" << c2 << endl;
+    string startDateStr(argv[9]);
+    //cout << "startDate str is:" << startDateStr << endl;
+    string endDateStr(argv[10]);
+    //cout << "endDate str is:" << endDateStr << endl;
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    chrono::year_month_day endDate = str2Date(endDateStr);
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;    
+}
+void handleMACD(int argc,char* argv[]){
+    string symbol(argv[2]);
+    int x = stoi(argv[3]);
+    string startDateStr(argv[4]);
+    string endDateStr(argv[5]);
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    chrono::year_month_day endDate = str2Date(endDateStr);
+    //cout << "symbol is:" << symbol << endl;
+    //cout << "x is:" << x << endl;
+    //cout << "startDate str is:" << startDateStr << endl;
+    //cout << "endDate str is:" << endDateStr << endl;
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;    
+}
+void handleRSI(int argc,char* argv[]){
+    string symbol(argv[2]);
+    //cout << "symbol is:" << symbol << endl;
+    int x = stoi(argv[3]);
+    //cout << "x is:" << x << endl;
+    int n = stoi(argv[4]);
+    //cout << "n is:" << n << endl;
+    int oversold = stoi(argv[5]);
+    //cout << "oversold is:" << oversold << endl;
+    int overbought = stoi(argv[6]);
+    //cout << "overbought is:" << overbought << endl;
+    string startDateStr(argv[7]);
+    //cout << "startDate str is:" << startDateStr << endl;
+    string endDateStr(argv[8]);
+    //cout << "endDate str is:" << endDateStr << endl;
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    chrono::year_month_day endDate = str2Date(endDateStr);    
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;    
+}
+void handleADX(int argc,char* argv[]){
+    string symbol(argv[2]);
+    int x = stoi(argv[3]);
+    int n = stoi(argv[4]);
+    int adx_threshold = stoi(argv[5]);
+    string startDateStr(argv[6]);
+    string endDateStr(argv[7]);
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    chrono::year_month_day endDate = str2Date(endDateStr);   
+    //cout << "symbol is:" << symbol << endl;
+    //cout << "x is:" << x << endl;
+    //cout << "n is:" << n << endl;
+    //cout << "adx_threshold is:" << adx_threshold << endl;
+    //cout << "startDate str is:" << startDateStr << endl;
+    //cout << "endDate str is:" << endDateStr << endl;
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;    
+}
+void handleRegression(int argc,char* argv[]){
+    string symbol(argv[2]);
+    //cout << "symbol is:" << symbol << endl;
+    int x = stoi(argv[3]);
+    //cout << "x is:" << x << endl;
+    int p = stoi(argv[4]);    
+    //cout << "p is:" << p << endl;
+    string trainStartStr(argv[5]);
+    //cout << "trainStartStr is:" << trainStartStr << endl;
+    string trainEndStr(argv[6]);
+    //cout << "trainEndStr is:" << trainEndStr << endl;
+    string startDateStr(argv[7]);
+    //cout << "startDate str is:" << startDateStr << endl;
+    string endDateStr(argv[8]);
+    //cout << "endDate str is:" << endDateStr << endl;
+    chrono::year_month_day trainStart = str2Date(trainStartStr);
+    //cout << "trainStart is:" << unsigned(trainStart.day()) << "/" << unsigned(trainStart.month()) << "/" << int(trainStart.year()) << endl;
+    chrono::year_month_day trainEnd = str2Date(trainEndStr);
+    //cout << "trainEnd is:" << unsigned(trainEnd.day()) << "/" << unsigned(trainEnd.month()) << "/" << int(trainEnd.year()) << endl;    
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    chrono::year_month_day endDate = str2Date(endDateStr);
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;        
+}
+void handleBestOfAll(int argc,char* argv[]){
+    string symbol(argv[2]);
+    string startDateStr(argv[3]);
+    string endDateStr(argv[4]);
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    chrono::year_month_day endDate = str2Date(endDateStr);
+    //cout << "startDate str is:" << startDateStr << endl;
+    //cout << "endDate str is:" << endDateStr << endl;
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;    
+}
+void handlePairs(int argc,char* argv[]){
+    string symbol1(argv[2]);
+    string symbol2(argv[3]);
+    int x = stoi(argv[4]);
+    int n = stoi(argv[5]);
+    int threshold = stoi(argv[6]);
+    string startDateStr(argv[7]);
+    string endDateStr(argv[8]);
+    chrono::year_month_day startDate = str2Date(startDateStr);
+    chrono::year_month_day endDate = str2Date(endDateStr);
+    //cout << "symbol1:" << symbol1 << endl;
+    //cout << "symbol2:" << symbol2 << endl;
+    //cout << "x:" << x << endl;
+    //cout << "n:" << n << endl;
+    //cout << "threshold:" << threshold << endl;
+    //cout << "startDateStr:" << startDateStr << endl;
+    //cout << "endDateStr:" << endDateStr << endl;
+    //cout << "startDate is:" << unsigned(startDate.day()) << "/" << unsigned(startDate.month()) << "/" << int(startDate.year()) << endl;
+    //cout << "endDate is:" << unsigned(endDate.day()) << "/" << unsigned(endDate.month()) << "/" << int(endDate.year()) << endl;        
+    if(argc == 9){
+
+    }else if(argc == 10){
+        int stop_loss_thres = stoi(argv[9]);
+        //cout << "stop_loss_thres:" << stop_loss_thres << endl;
+    }else{
+        //cout << "Wrong number of arguments for pair" << endl;
+    }
+}
 int main(int argc,char* argv[]){
-    for(int i=0;i<argc;i++){
-        std::cout << argv[i] << endl;
+    string startegyName(argv[1]);
+    if(startegyName == "BASIC"){
+        handleBASIC(argc,argv);           
+    }else 
+    if(startegyName == "DMA"){
+        handleDMA(argc,argv);
+    }else
+    if(startegyName == "DMA++"){
+        handleDMAplus(argc,argv);
+    }else 
+    if(startegyName == "MACD"){
+        handleMACD(argc,argv);
+    }else 
+    if(startegyName == "RSI"){
+        handleRSI(argc,argv);
+    }else 
+    if(startegyName == "ADX"){
+        handleADX(argc,argv);
+    }else 
+    if(startegyName == "LINEAR_REGRESSION"){
+        handleRegression(argc,argv);
+    }else 
+    if(startegyName == "BEST_OF_ALL"){
+        handleBestOfAll(argc,argv);
+    }else 
+    if(startegyName == "PAIRS"){
+        handlePairs(argc,argv);
+    }else{
+        //cout << "Invalid strategy" << endl;
     }
 }
