98dd73d6d93b5994820548cd2893fcfc8062ee18
Siddhartha Singh <sidsiskilled@gmail.com>
2024-02-08 19:45:48 +0530

    Small commit containg dateutil.h to hande daay subtraction

--- src/dateUtil.h
+++ src/dateUtil.h
@@ -1,7 +1,40 @@
 #include <chrono>
 #include <iostream>
 #include <ctime>
+#include "parser.h"
 using namespace std;
+
+PriceTable getPriceTable(string& symbolName,chrono::year_month_day startDate,chrono::year_month_day endDate){
+    string command = "python3 ./src/stockData.py ";
+    command += symbolName + " ";
+    int startYear,startMonth,startDay;
+    startYear = int(startDate.year());
+    startMonth = unsigned(startDate.month());
+    startDay = unsigned(startDate.day());
+    
+    cout << "startYear:" << startYear << " " << "startMonth:" << startMonth << " startDay:" << startDay << endl;
+
+    int endYear,endMonth,endDay;
+    endYear = int(endDate.year());
+    endMonth = unsigned(endDate.month());
+    endDay = unsigned(endDate.day());
+    string filePath = CSV_FILE_PATH + symbolName + ".csv";
+    
+    cout << "endYear:" << endYear << " " << "endMonth:" << endMonth << " endDay:" << endDay << endl;
+    command += to_string(startYear) + " " + to_string(startMonth) + " " + to_string(startDay) + " ";
+    command += to_string(endYear) + " " + to_string(endMonth) + " " + to_string(endDay) + " ";
+    command += filePath + " ";
+
+    cout << "command is:" << command << endl;
+    system(command.c_str());
+
+    PriceTable table;
+    table.read_csv(filePath);
+    /*for(PriceTableRow& row:table.rows){
+        cout << row.getString() << endl;
+    } */ 
+    return std::move(table);  
+}
 
 chrono::year_month_day subtractDate(chrono::year_month_day date,int n){
     chrono::time_point time = chrono::sys_days{date};
--- src/main.cpp
+++ src/main.cpp
@@ -9,37 +9,7 @@ using namespace std;
 #define CSV_FILE_PATH "./bin/stockCSV/"
 
 //path in terminal here same as from where the exe is run
-PriceTable getPriceTable(string& symbolName,chrono::year_month_day startDate,chrono::year_month_day endDate){
-    string command = "python3 ./src/stockData.py ";
-    command += symbolName + " ";
-    int startYear,startMonth,startDay;
-    startYear = int(startDate.year());
-    startMonth = unsigned(startDate.month());
-    startDay = unsigned(startDate.day());
-    
-    cout << "startYear:" << startYear << " " << "startMonth:" << startMonth << " startDay:" << startDay << endl;
 
-    int endYear,endMonth,endDay;
-    endYear = int(endDate.year());
-    endMonth = unsigned(endDate.month());
-    endDay = unsigned(endDate.day());
-    string filePath = CSV_FILE_PATH + symbolName + ".csv";
-    
-    cout << "endYear:" << endYear << " " << "endMonth:" << endMonth << " endDay:" << endDay << endl;
-    command += to_string(startYear) + " " + to_string(startMonth) + " " + to_string(startDay) + " ";
-    command += to_string(endYear) + " " + to_string(endMonth) + " " + to_string(endDay) + " ";
-    command += filePath + " ";
-
-    cout << "command is:" << command << endl;
-    system(command.c_str());
-
-    PriceTable table;
-    table.read_csv(filePath);
-    /*for(PriceTableRow& row:table.rows){
-        cout << row.getString() << endl;
-    } */ 
-    return std::move(table);  
-}
 
 
 void handleBASIC(int argc,char* argv[]){
